92434eec9cb1591fea8b04b2a6487a022ef8e5fa
Kavtorev <dmitrijkavtorev@gmail.com>
2021-04-13 18:30:33 +0200

    ColumnForm improved reusability

--- src/components/ColumnForm.js
+++ src/components/ColumnForm.js
@@ -3,16 +3,27 @@ import React from "react";
 import TextField from "@material-ui/core/TextField";
 import InputAdornment from "@material-ui/core/InputAdornment";
 
-export default function ColumnForm({ endAdornment = null, ...rest }) {
+export default function ColumnForm({
+  startAdornment = null,
+  children = null,
+  endAdornment = null,
+  ...rest
+}) {
   return (
     <TextField
       required
       defaultValue="0"
       InputProps={{
-        startAdornment: <InputAdornment position="start">s.</InputAdornment>,
+        startAdornment: startAdornment ? (
+          startAdornment
+        ) : (
+          <InputAdornment position="start">s.</InputAdornment>
+        ),
         endAdornment,
       }}
       {...rest}
-    />
+    >
+      {children}
+    </TextField>
   );
 }
