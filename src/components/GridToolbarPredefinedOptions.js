a9539d932a77d253ed89c99cf5a6f16c423aabb5
Kavtorev <dmitrijkavtorev@gmail.com>
2021-04-13 18:51:08 +0200

    Upload CSV menu

--- src/components/GridToolbarPredefinedOptions.js
+++ src/components/GridToolbarPredefinedOptions.js
@@ -6,13 +6,43 @@ import {
 } from "@material-ui/data-grid";
 import PublishOutlinedIcon from "@material-ui/icons/PublishOutlined";
 import Button from "@material-ui/core/Button";
+import { Menu, MenuItem } from "@material-ui/core";
 function GridToolBarImport() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <>
-      <Button aria-label="Upload data" size="small">
+      <Button
+        aria-controls="import-csv"
+        aria-haspopup="true"
+        aria-label="Upload data"
+        size="small"
+        onClick={handleClick}
+      >
         <PublishOutlinedIcon />
         Import
       </Button>
+      <Menu
+        id="import-csv"
+        anchorEl={anchorEl}
+        keepMounted
+        getContentAnchorEl={null}
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "left",
+        }}
+      >
+        <MenuItem onClick={handleClose}>Upload CSV</MenuItem>
+      </Menu>
     </>
   );
 }
