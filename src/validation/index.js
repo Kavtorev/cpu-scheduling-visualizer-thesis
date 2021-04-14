3a4d9ed8bd199fa2b33658dc6ca4d8424a0a16c6
Kavtorev <dmitrijkavtorev@gmail.com>
2021-04-14 16:07:28 +0200

    validation stuff

---
+++ src/validation/index.js
@@ -0,0 +1,45 @@
+import * as yup from "yup";
+
+export const validate = (schema) => (value) => {
+  let errors;
+  try {
+    schema.validateSync(value);
+  } catch (error) {
+    if (error.errors) {
+      errors = error.errors.join(" ");
+    }
+  }
+  return errors;
+};
+
+export const arrivalTimeSchema = yup
+  .number()
+  .typeError("Please provide a number.")
+  .min(0)
+  .max(10)
+  .required()
+  .integer();
+
+export const cpuBurstSchema = yup
+  .number()
+  .typeError("Please provide a number.")
+  .min(1)
+  .max(20)
+  .required()
+  .integer();
+
+export const prioritySchema = yup
+  .number()
+  .typeError("Please provide a number.")
+  .min(1)
+  .max(10)
+  .required()
+  .integer();
+
+export const inputOutputSchema = yup
+  .number()
+  .typeError("Please provide a number.")
+  .min(1)
+  .max(20)
+  .required()
+  .integer();
