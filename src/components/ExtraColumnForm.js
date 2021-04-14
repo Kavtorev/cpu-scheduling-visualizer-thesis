0ecf04ed06ed61d933c1351814066ae88cdc2b2d
Kavtorev <dmitrijkavtorev@gmail.com>
2021-04-14 16:09:44 +0200

    ExtraColumnForm logic

--- src/components/ExtraColumnForm.js
+++ src/components/ExtraColumnForm.js
@@ -1,16 +1,28 @@
 import React from "react";
 import InputAdornment from "@material-ui/core/InputAdornment";
 import RemoveRoundedIcon from "@material-ui/icons/RemoveRounded";
-import ExtraColumnButton from "./ExtraColumnButton";
+import Button from "@material-ui/core/Button";
+import { getIsExtraRequiredById, removeColumn } from "../redux/ui/uiSlice";
 import ColumnForm from "./ColumnForm";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function ExtraColumnForm({ buttonId, ...rest }) {
-  const endAdornment = (
+  const dispatch = useDispatch();
+  const isRequired = useSelector(getIsExtraRequiredById);
+
+  const handleRemoveColumn = (e) =>
+    dispatch(
+      removeColumn({
+        id: e.currentTarget.id,
+        label: e.currentTarget.getAttribute("label"),
+      })
+    );
+  const endAdornment = !isRequired[buttonId] ? (
     <InputAdornment position="end">
-      <ExtraColumnButton id={buttonId} label={rest.label}>
+      <Button id={buttonId} label={rest.label} onClick={handleRemoveColumn}>
         <RemoveRoundedIcon />
-      </ExtraColumnButton>
+      </Button>
     </InputAdornment>
-  );
+  ) : null;
   return <ColumnForm endAdornment={endAdornment} {...rest} />;
 }
