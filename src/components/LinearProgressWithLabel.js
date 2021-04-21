03156bbb3318264e3e07be275e0fc34530565395
Kavtorev <dmitrijkavtorev@gmail.com>
2021-04-21 19:50:09 +0200

    dynamic LinearProgress

--- src/components/LinearProgressWithLabel.js
+++ src/components/LinearProgressWithLabel.js
@@ -2,16 +2,22 @@ import React from "react";
 import LinearProgress from "@material-ui/core/LinearProgress";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
+import { useSelector } from "react-redux";
+import { getIndex, getNumberOfFrames } from "../redux/player/playerSlice";
 
 export default function LinearProgressWithLabel(props) {
+  const index = useSelector(getIndex);
+  const numFrames = useSelector(getNumberOfFrames);
+  const percentage = (100 / numFrames) * index || 0;
+
   return (
     <Box display="flex" alignItems="center">
       <Box width="100%" mr={1}>
-        <LinearProgress variant="determinate" {...props} />
+        <LinearProgress variant="determinate" value={percentage} />
       </Box>
       <Box minWidth={35}>
         <Typography variant="body2" color="textSecondary">{`${Math.round(
-          props.value
+          percentage
         )}%`}</Typography>
       </Box>
     </Box>
