5744cb213d84c1143ebe6bf40bbd9b62a7667c38
Kavtorev <dmitrijkavtorev@gmail.com>
2021-04-22 21:37:18 +0200

    animated metrics

--- src/components/PlayerMetrics.js
+++ src/components/PlayerMetrics.js
@@ -2,12 +2,13 @@ import React from "react";
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
 import ColumnForm from "./DataForm/TextInput";
-
 import {
   AverageResponseTime,
   AverageWaitingIcon,
   AverageTurnaroundIcon,
 } from "./PlayerMetricsIcons";
+import { useSelector } from "react-redux";
+import { getMetrics } from "../redux/player/playerSlice";
 
 const useStyles = makeStyles((theme) => ({
   metricFormRoot: {
@@ -20,24 +21,38 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const metricsMeta = [
-  { label: "Average Turnaround Time", icon: <AverageTurnaroundIcon /> },
-  { label: "Average Waiting Time", icon: <AverageWaitingIcon /> },
-  { label: "Average Response Time", icon: <AverageResponseTime /> },
-];
-
 export default function PlayerMetrics() {
   const styles = useStyles();
+  const metrics = useSelector(getMetrics);
+
+  const metricsMeta = [
+    {
+      label: "Average Turnaround Time",
+      icon: <AverageTurnaroundIcon />,
+      value: metrics[0],
+    },
+    {
+      label: "Average Waiting Time",
+      icon: <AverageWaitingIcon />,
+      value: metrics[1],
+    },
+    {
+      label: "Average Response Time",
+      icon: <AverageResponseTime />,
+      value: metrics[2],
+    },
+  ];
+
   return (
     <Grid container spacing={2}>
-      {metricsMeta.map((e) => {
+      {metricsMeta.map((e, i) => {
         return (
           <Grid item key={e.label}>
             <ColumnForm
               label={e.label}
               startAdornment={e.icon}
               classes={{ root: styles.metricFormRoot }}
-              defaultValue="0 s."
+              value={`${e.value.toFixed(2)} s.`}
               disabled
             />
           </Grid>
