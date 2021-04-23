82fbb72d3be9e8f0059310c4959c88a1dbe63baa
Kavtorev <dmitrijkavtorev@gmail.com>
2021-04-23 10:21:53 +0200

    unnnecessary code

--- src/algos/comparators.js
+++ src/algos/comparators.js
@@ -3,22 +3,4 @@ const fifoComparator = (a, b) => {
   else return -1;
 };
 
-const sjfComparator = (a, b) => {
-  if (b.arrivalTime === 0) return 1;
-  if (a.cpuTime > b.cpuTime) return 1;
-  if (a.cpuTime === b.cpuTime) {
-    if (a.arrivalTime > b.arrivalTime) return 1;
-    else return -1;
-  } else return -1;
-};
-
-const priorityComparator = (a, b) => {
-  if (b.arrivalTime === 0) return 1;
-  if (a.priority > b.priority) return 1;
-  if (a.priority === b.priority) {
-    if (a.arrivalTime > b.arrivalTime) return 1;
-    else return -1;
-  } else return -1;
-};
-
-export { fifoComparator, sjfComparator, priorityComparator };
+export { fifoComparator };
--- src/algos/helpers/index.js
+++ src/algos/helpers/index.js
@@ -1,47 +1,3 @@
-export const getAverageTimes = (finishedPr) => {
-  let sumTurnaroundTimes = 0;
-  let sumWaitingTimes = 0;
-  let sumResponseTime = 0;
-  let length = finishedPr.length;
-
-  for (let p of finishedPr) {
-    sumTurnaroundTimes += p.turnaroundTime;
-    sumWaitingTimes += p.waitingTime;
-    sumResponseTime += p.responseTime;
-  }
-
-  return {
-    avgTurnaroundTime: sumTurnaroundTimes / length,
-    avgWaitingTime: sumWaitingTimes / length,
-    avgResponseTime: sumResponseTime / length,
-  };
-};
-
-// let frame = {
-//   start: 0,
-//   finish: 0,
-//   startState: "Running",
-//   finishState: "",
-//   process: null,
-// };
-
-let frame = {
-  start: {
-    time: 0,
-    state: "running",
-    process: null,
-  },
-  finish: {
-    time: 0,
-    state: "",
-    process: null,
-  },
-};
-
-export const startFrame = (clock, process) => {
-  return { ...frame, start: { ...frame.start, time: clock, process } };
-};
-
 export let s = (clock, process) => {
   return {
     start: {
@@ -74,25 +30,4 @@ export let f = (frame, clock, state, process) => {
       },
     },
   };
-};
-
-export const finishFrame = (frame, state, clock, current, previous = null) => {
-  // let res = {
-  //   ...frame,
-  //   finishState: state,
-  //   finish: clock,
-  //   process: { ...process },
-  // };
-  let res = {
-    ...frame,
-    finish: {
-      time: clock,
-      state,
-      process: { ...previous },
-    },
-  };
-  // starts again
-  frame.start.time = clock;
-
-  return res;
 };
